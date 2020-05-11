b384be95879805deaa14ee5a945d05b063270c1f
sculouis <scu.louis@gmail.com>
2020-05-11 22:07:20 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <hello-world v-model="Subject"></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: "App",
+  components: { HelloWorld },
+  data() {
+    return { Subject: "" };
+  },
+  watch: {
+    Subject: function() {
+      console.log(this.Subject);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
