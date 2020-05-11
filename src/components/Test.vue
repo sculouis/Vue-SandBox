185cd79edd44ed867de4bf2b0ab31301b86c97ed
sculouis <scu.louis@gmail.com>
2020-05-11 22:20:56 +0800

    change content

---
+++ src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="val">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputText",
+  data: function() {
+    return { val: "" };
+  },
+  mounted: function() {
+    console.log(this.value);
+    this.val = this.value;
+  },
+  updated: function() {
+    this.$emit("input", this.val);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
